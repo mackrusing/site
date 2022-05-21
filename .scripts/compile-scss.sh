d52d18d4ecd75e0ec47e8e5a6321976e4114ed50
Mack Rusing <mackrusing@gmail.com>
2022-05-20 23:08:48 -0700

    Update structure

--- .scripts/compile-scss.sh
+++
@@ -1,1 +0,0 @@
-sass --watch resources/styles/style.scss resources/styles/style.css --no-source-map
---
+++ main.js
@@ -0,0 +1,32 @@
+// packages & std library
+const express = require('express');
+const path = require('path');
+
+// port
+const PORT = process.env.PORT || 8080;
+
+// app instance
+const app = express();
+
+// static files
+app.use('/resources', express.static('public'));
+
+// handle get reqs
+app
+  .get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/html/home.html'));
+  })
+  .get('/info', (req, res) => {
+    res.sendFile(path.join(__dirname, '/html/info.html'));
+  });
+
+// not found
+app.use((req, res) => {
+  res.status(404);
+  res.sendFile(path.join(__dirname, '/html/404.html'));
+});
+
+// listen on port
+app.listen(PORT, () => {
+  console.log(`listening for connections on http://localhost:${PORT}`);
+});
