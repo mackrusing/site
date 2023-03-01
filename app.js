07e4188986927b9342ff3d8f918b34f07c05862b
Mack Rusing <dev@mackrusing.com>
2023-02-28 19:12:08 -0700

    replace vanilla html with pug

--- app.js
+++ app.js
@@ -7,8 +7,19 @@ const PORT = 8000;
 // app instance
 const app = express();
 
+// template engine
+app.set("view engine", "pug");
+app.set("views", "./views");
+
 // static files
-app.use("/", express.static("public"));
+app.use("/resources", express.static("./resources"));
+
+app.get("/", (request, response) => {
+    response.render("home", {});
+});
+app.get("/about", (request, response) => {
+    response.render("about", {});
+});
 
 // launch server
 app.listen(PORT, () => {
